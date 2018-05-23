bd99205633237a67a468fb3e6229e0667bb45b0d
Hanh Nguyen <hanh.nguyentai@drcomgroup.com>
2018-05-23 18:20:47 +0700

    update file

--- 02ReduxCombineReducer/src/app.js
+++ 02ReduxCombineReducer/src/app.js
@@ -1,5 +1,12 @@
 "use strict"
 
+// REACT
+import React from 'react';
+import {render} from 'react-dom';
+import {Provider} from 'react-redux';
+
+
+// MIDDLEWARE
 import {applyMiddleware, createStore} from 'redux';
 
 import logger from 'redux-logger';
@@ -20,6 +27,14 @@ const store = createStore(reducers, middleware);
 // store.subscribe(function(){
 // 	console.log('current state is: ', store.getState());
 // })
+
+import BooksList from './components/pages/booksList';
+
+render(
+	<Provider store={store}>
+	<BooksList />
+	</Provider>, document.getElementById('app')
+);
 
 store.dispatch(postBooks(
 	[{
@@ -43,24 +58,24 @@ store.dispatch(postBooks(
 	]
 ))
 
-store.dispatch(deleteBooks({id: 3}))
+// store.dispatch(deleteBooks({id: 3}))
 
-store.dispatch(updateBooks(
-	{
-		id: 2,
-		title: 'title new update 2'
-	}
-))
+// store.dispatch(updateBooks(
+// 	{
+// 		id: 2,
+// 		title: 'title new update 2'
+// 	}
+// ))
 
 // delete a book
 
 //-->> CART ACTIONS <<--
 // ADD TO CART
-store.dispatch(addToCart([{
-	id: 1,
-	title: 'This is the book title',
-	description: 'This is the book description',
-	price: 33.44
-}]
-))
+// store.dispatch(addToCart([{
+// 	id: 1,
+// 	title: 'This is the book title',
+// 	description: 'This is the book description',
+// 	price: 33.44
+// }]
+// ))
 	//-------------------------------------------
